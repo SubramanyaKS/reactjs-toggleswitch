8b2f9c8825384a77a2767d0d8a00be50119ecb3c
Subramanya K S <subramanyaks2205@gmail.com>
2024-02-27 16:35:26 +0530

    added: aria-label released 1.1.2

--- src/components/ToggleSwitch/ToggleSwitch.tsx
+++ src/components/ToggleSwitch/ToggleSwitch.tsx
@@ -8,16 +8,17 @@ interface ToggleSwitchProps {
   onColor?:string;
   offColor?:string;
   disable?:boolean;
+  ariaLabel?:string;
 }
 
-const ToggleSwitch: React.FC<ToggleSwitchProps> = ({ checked, onToggle,onColor="#2196f3",offColor="#cccccc",disable=false }) => {
+const ToggleSwitch: React.FC<ToggleSwitchProps> = ({ checked, onToggle,onColor="#2196f3",offColor="#cccccc",disable=false,ariaLabel='' }) => {
   const handleChange = () => {
     onToggle(!checked);
   };
 
   return (
     <label className='toggle-switch'>
-      <input type='checkbox' disabled={disable} checked={checked} onChange={handleChange} />
+      <input type='checkbox' disabled={disable} checked={checked} onChange={handleChange} aria-label={ariaLabel} />
       <span style={{ backgroundColor: checked ? onColor : offColor }} className='slider'></span>
     </label>
   );
